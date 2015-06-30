bce3083d74d89039aa814e3c110cbe28842670c2
Gabriel Castillo <gabriel.castillo86@hotmail.com>
2015-06-29 22:54:24 -0500

    Se agrega proyecto de android para pacientes

---
+++ android/MigranaPaciente/app/src/main/java/paciente/android/umbrella/uniandes/edu/co/aplications/Globals.java
@@ -0,0 +1,19 @@
+//package paciente.android.umbrella.uniandes.edu.co.aplications;
+//
+//import android.app.Application;
+//
+///**
+// * Created by Gabriel on 29/06/2015.
+// */
+//public class Globals extends Application {
+//
+//    private String usernameAuthenticated;
+//
+//    public String getUsernameAuthenticated() {
+//        return usernameAuthenticated;
+//    }
+//
+//    public void setUsernameAuthenticated(String usernameAuthenticated) {
+//        this.usernameAuthenticated = usernameAuthenticated;
+//    }
+//}
---
+++ android/MigranaPaciente/app/src/main/java/paciente/android/umbrella/uniandes/edu/co/entities/User.java
@@ -0,0 +1,26 @@
+package paciente.android.umbrella.uniandes.edu.co.entities;
+
+/**
+ * Created by Gabriel on 27/06/2015.
+ */
+public class User {
+    public String Identification;
+
+    public String getIdentification() {
+        return Identification;
+    }
+
+    public void setIdentification(String identification) {
+        Identification = identification;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String Password;
+}
