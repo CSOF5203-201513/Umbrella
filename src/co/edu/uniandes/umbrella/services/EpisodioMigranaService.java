a773c621430939392f51d44c2fda033bbcd2805d
ericaprado <erica.estefanny@gmail.com>
2015-07-01 20:57:13 -0500

    Ajustes Persistence

--- src/co/edu/uniandes/umbrella/services/EpisodioMigranaService.java
+++ src/co/edu/uniandes/umbrella/services/EpisodioMigranaService.java
@@ -3,24 +3,24 @@
  */
 package co.edu.uniandes.umbrella.services;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
+import javax.transaction.UserTransaction;
 import javax.ws.rs.GET;
-import javax.ws.rs.OPTIONS;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.json.simple.JSONObject;
 
 import co.edu.uniandes.umbrella.entity.EpisodioMigrana;
+import co.edu.uniandes.umbrella.entity.PersistenceManager;
 
 /**
  * @author erica.prado
@@ -31,6 +31,10 @@ import co.edu.uniandes.umbrella.entity.EpisodioMigrana;
 @Produces(MediaType.APPLICATION_JSON)
 public class EpisodioMigranaService {
 
+	
+	@PersistenceContext(unitName = "ControlMigrana")
+    EntityManager entityManager;
+	
 	
 	/**
 	 * Permite obtener la lista completa de personas
@@ -46,10 +50,58 @@ public class EpisodioMigranaService {
 	/**
 	 * Permite agregar una nueva persona
 	 * @return respuesta de la peticion
+	 * @throws SystemException 
+	 * @throws NotSupportedException 
 	 */
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response add() {
+	public Response add() throws NotSupportedException, SystemException {
+		
+		try {
+            entityManager = PersistenceManager.getInstance().getEntityManagerFactory().createEntityManager();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+		
+		
+			
+		EpisodioMigrana e = new EpisodioMigrana();
+              
+        
+        e.setEstado(1);
+        e.setIdIntensidad(1);
+        e.setIdlocalizacionDolor(1);
+        e.setIdMedico(1);
+        e.setIdPaciente(1);
+        e.setRutaAudio("");  
+		
+        
+        try {
+        	
+        	UserTransaction transaction = (UserTransaction)new InitialContext().lookup("java:comp/UserTransaction");
+        	transaction.begin();
+            
+        	//Prueba Insert
+            //entityManager.persist(e);
+
+            //Prueba Consult
+        	//EpisodioMigrana prueba = entityManager.find(EpisodioMigrana.class, 1);
+        	
+            transaction.commit();
+           
+        } catch (Throwable t) {
+            t.printStackTrace();
+            if (entityManager.getTransaction().isActive()) {
+                entityManager.getTransaction().rollback();
+            }
+            e = null;
+        } finally {
+        	entityManager.clear();
+        	entityManager.close();
+        }
+        
+        
+		
 
 		JSONObject rta = new JSONObject();
 		rta.put("success", "true");
