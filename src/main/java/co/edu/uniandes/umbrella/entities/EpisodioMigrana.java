99384a9370a81fdd4b37093934ba56b7dc4cdf44
Oscar Javier Moreno Rey <ojmorenor@gmail.com>
2015-07-04 12:55:14 -0500

    Definición de estrategia para autogenerar id en EpisodioMigrana entity

--- src/main/java/co/edu/uniandes/umbrella/entities/EpisodioMigrana.java
+++ src/main/java/co/edu/uniandes/umbrella/entities/EpisodioMigrana.java
@@ -10,6 +10,8 @@ import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
@@ -43,6 +45,7 @@ public class EpisodioMigrana implements Serializable {
     @Id
     @Basic(optional = false)
     @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Integer id;
     @Column(name = "estado")
