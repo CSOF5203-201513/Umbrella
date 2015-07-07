7905b3e4afdcdeaa771ddfc48f251758baecf217
unknown <erica.estefanny@gmail.com>
2015-07-06 19:16:03 -0500

    Servicios REST Usuario y Lista

---
+++ src/rest/src/main/java/co/edu/uniandes/umbrellarest/service/ListaFacadeREST.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package co.edu.uniandes.umbrellarest.service;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import co.edu.uniandes.negocio.Lista;
+
+/**
+ * @author Erica
+ *
+ */
+
+@Stateless
+@Path("listas")
+public class ListaFacadeREST extends AbstractFacade<Lista>{
+	
+	
+	 @PersistenceContext(unitName = "ControlMigrana")
+	    private EntityManager em;
+
+	    public ListaFacadeREST() {
+	        super(Lista.class);
+	    }
+	    
+	    @POST
+	    @Override
+	    @Consumes({"application/xml", "application/json"})
+	    public void create(Lista entity) {
+	        super.create(entity);
+	    }
+
+	    @PUT
+	    @Path("{id}")
+	    @Consumes({"application/xml", "application/json"})
+	    public void edit(@PathParam("id") Integer id, Lista entity) {
+	        super.edit(entity);
+	    }
+
+	    @DELETE
+	    @Path("{id}")
+	    public void remove(@PathParam("id") Integer id) {
+	        super.remove(super.find(id));
+	    }
+
+	    @GET
+	    @Path("{id}")
+	    @Produces({"application/xml", "application/json"})
+	    public Lista find(@PathParam("id") Integer id) {
+	        return super.find(id);
+	    }
+
+	    @GET
+	    @Override
+	    @Produces({"application/xml", "application/json"})
+	    public List<Lista> findAll() {
+	        return super.findAll();
+	    }
+
+	    @GET
+	    @Path("{from}/{to}")
+	    @Produces({"application/xml", "application/json"})
+	    public List<Lista> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+	        return super.findRange(new int[]{from, to});
+	    }
+
+	    @GET
+	    @Path("count")
+	    @Produces("text/plain")
+	    public String countREST() {
+	        return String.valueOf(super.count());
+	    }
+
+	    @Override
+	    protected EntityManager getEntityManager() {
+	        return em;
+	    }
+
+}
